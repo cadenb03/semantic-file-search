a8b242ccbec6c3a988374a0e7e3096a588adf822
cadenb03 <caden.boyd10@gmail.com>
2026-03-25 14:09:07 -0700

    first commit

--- test.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-text_chunks = ["The Linux kernel is open source.", "Tux is the official mascot."]
-embeddings = model.encode(text_chunks)
